6efd10348d4014ab5fd3f52e32df5aec087b68af
hoxiansen <1365171821@qq.com>
2019-05-11 18:17:20 +0800

    push

--- src/main/java/com/hoxiansen/controller/AdminController.java
+++ src/main/java/com/hoxiansen/controller/AdminController.java
@@ -2,12 +2,19 @@ package com.hoxiansen.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller("/admin")
+@Controller
+@RequestMapping("/admin")
 public class AdminController {
 
     @GetMapping("/")
     public String index() {
         return "admin/index";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "admin/login";
     }
 }
